b26dc2637ed08afc18b1588f6aa5fe2ca7f1e565
knightykc <knightykc@gmail.com>
2020-10-31 16:19:51 -0700

    add UML with bean to compare with the UML with SPeL

--- src/main/java/com/cyang/statemachine/UmlStateMachine.java
+++ src/main/java/com/cyang/statemachine/UmlStateMachine.java
@@ -2,6 +2,7 @@ package com.cyang.statemachine;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachine;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineModelConfigurer;
@@ -22,6 +23,14 @@ public class UmlStateMachine extends StateMachineConfigurerAdapter<String, Strin
     @Bean
     public StateMachineModelFactory<String, String> modelFactoryWithEffect() {
         // return new UmlStateMachineModelFactory("classpath:with-effect.uml");
-        return new UmlStateMachineModelFactory("classpath:without-effect.uml");
+        return new UmlStateMachineModelFactory("classpath:with-effect.uml");
+        // return new UmlStateMachineModelFactory("classpath:with-effect-bean.uml");
+    }
+
+    @Bean(name="effectaction")
+    public Action<String, String> effectAction() {
+        return context -> {
+            System.out.println("Spring bean works in effect in UML");
+        };
     }
 }
